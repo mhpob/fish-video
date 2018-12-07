21400f936918103e3a7e1d644c75f26bb60656f5
mhpob <mikeob9@gmail.com>
2018-12-06 19:08:01 -0500

    Play with background removal.

--- Code/background_remove.py
+++ Code/background_remove.py
@@ -1,13 +1,29 @@
+
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture('vtest.avi')
-fgbg = cv.createBackgroundSubtractorMOG()
+cap = cv.VideoCapture('p:/obrien/biotelemetry/darpa/fish-video/bsb_vid.mp4')
+fgbg = cv.createBackgroundSubtractorMOG2(detectShadows = False)
+
+fourcc = cv.VideoWriter_fourcc(*'mp4v')
+output_framesize = (int(cap.read()[1].shape[1] ),
+  int(cap.read()[1].shape[0] ))
+
+out = cv.VideoWriter(filename = 'p:/obrien/biotelemetry/darpa/fish-video/tshort_bg.mp4',
+  fourcc = fourcc, fps = 24.0, frameSize = output_framesize, isColor = False)
+
 while(1):
   ret, frame = cap.read()
-fgmask = fgbg.apply(frame)
-cv.imshow('frame',fgmask)
-k = cv.waitKey(30) & 0xff
-if k == 27:
-  break
+  if ret == True:
+    fgmask = fgbg.apply(frame)
+    # out.write(fgmask)
+    cv.imshow('frame',fgmask)
+
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+      break
+
+
 cap.release()
 cv.destroyAllWindows()
+
+
